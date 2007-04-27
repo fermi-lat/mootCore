542a961deddca18fd28f5d2ab8e00e19ea121642
jrb <>
2007-04-27 23:52:09 +0000

    Add columns precinct_fk, active_state.  delegate_fk is deprecated

--- maint/createParameter_class.sql
+++ maint/createParameter_class.sql
@@ -1,10 +1,18 @@
-## Don't yet know if we need a Parameter_class table, but make it anyway
-## just so there is a key to refer to.
+##  Should get rid of delegate_fk one of these days.  Precinct_fk is the
+## important thing
 
 create table Parameter_class
 (Parameter_class_key INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
   name              VARCHAR(32) NOT NULL UNIQUE,
   description       VARCHAR(255) NOT NULL,
+  active_state      ENUM('ACTIVE','INACTIVE') NOT NULL default 'ACTIVE'
+    COMMENT 'this field allows for evolution of collection of Parameter classes',
+  precinct_fk INT UNSIGNED  COMMENT 
+   'who manages this parameter class',
+  FOREIGN KEY(precinct_fk) REFERENCES Precincts (precinct_key)
+  ON DELETE CASCADE
+  ON UPDATE CASCADE
+
   delegate_fk INT UNSIGNED  COMMENT 
    'who manages this parameter class',
   FOREIGN KEY(delegate_fk) REFERENCES Delegates (delegate_key)
