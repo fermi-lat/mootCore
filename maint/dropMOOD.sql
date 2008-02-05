65d4a5dbb01918f7b6983e7422f9ef813dac6af7
panetta <>
2008-02-05 09:20:09 +0000

    fix drop statements

--- maint/dropMOOD.sql
+++ maint/dropMOOD.sql
@@ -5,42 +5,43 @@
 ## drop relation tables first.  Comment out drop commands for planned tables
 ## that do not exist yet
 
-drop table Configs_to_FSW;
-drop table FSW_to_Parameters;
-drop table Configs_to_Parameters;
+drop table if exists Configs_to_FSW;
+drop table if exists FSW_to_Parameters;
+drop table if exists Configs_to_Parameters;
 
-drop table FClass_to_PClass;
-drop table OClass_to_AClass;
-drop table Vote_PClass_AClass;
-drop table Parameters_to_Ancillary;
-drop table Offline_to_Ancillary;
-drop table Container_Precinct;
-
-### drop grouping tables
-drop table Configs;
+drop table if exists FClass_to_PClass;
+drop table if exists OClass_to_AClass;
+drop table if exists Vote_PClass_AClass;
+drop table if exists Parameters_to_Ancillary;
+drop table if exists Offline_to_Ancillary;
+drop table if exists Container_Precinct;
 
 ### drop alias tables
-drop table Vote_aliases;
-drop table Ancillary_aliases;
+drop table if exists Vote_aliases;
+drop table if exists Ancillary_aliases;
 
-
+### drop grouping tables
+drop table if exists Acq_mode;
+drop table if exists Acq_summary;
+drop table if exists Configs;
 
 ### drop instance tables
-drop table FSW_inputs;
-drop table Parameters;
-drop table Votes;
-drop table Ancillary;
-drop table OfflineCalib;
+drop table if exists FSW_inputs;
+drop table if exists Parameters;
+drop table if exists Votes;
+drop table if exists Ancillary;
+drop table if exists OfflineCalib;
 
-drop table LATC_Backup;
-drop table Parameter_class;
+drop table if exists LATC_Backup;
+drop table if exists Parameter_class;
 
-drop table Precincts;
-drop table Instrument;
+drop table if exists Precincts;
+drop table if exists Instrument;
 
 ###drop class tables
-drop table FSW_class;
-drop table Offline_class;
-drop table Ancillary_class;
+drop table if exists FSW_class;
+drop table if exists Offline_class;
+drop table if exists Ancillary_class;
 
-drop table Delegates;
+drop table if exists Delegates;
+
