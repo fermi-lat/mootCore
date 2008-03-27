01dc389e2df9535dc9ad4c941e9c6103f296b757
jrb <>
2008-03-27 23:48:38 +0000

    FileDescrip: Add ConstitInfo class MootQuery:   Add routines to ask for constituent information test_queries: Test new queries

--- mootCore/FileDescrip.h
+++ mootCore/FileDescrip.h
@@ -1,4 +1,4 @@
-// $Header: /nfs/slac/g/glast/ground/cvs/mootCore/mootCore/FileDescrip.h,v 1.6 2007/12/07 20:02:44 jrb Exp $
+// $Header: /nfs/slac/g/glast/ground/cvs/mootCore/mootCore/FileDescrip.h,v 1.7 2008/02/13 02:16:12 panetta Exp $
 
 #ifndef MOOT_FileDescrip_h
 #define MOOT_FileDescrip_h
@@ -131,6 +131,48 @@ namespace MOOT {
     std::string m_creationTime;
     std::string m_voteKey;
   };                                 // end ConfigInfo
+
+  /** @class ConstitInfo
+        Information concerning flight software constituents    
+   */
+  class ConstitInfo {
+  public:
+    ConstitInfo(const std::string& key = std::string(""),
+                const std::string& name = std::string(""),
+                const std::string& version = std::string(""),
+                const std::string& pkg = std::string(""),
+                const std::string& dir = std::string(""),
+                const std::string& fmxPath = std::string(""),
+                const std::string& srcPath = std::string(""),
+                const std::string& fswId = std::string(""),
+                const std::string& status = std::string("")) :
+      m_key(key), m_name(name), m_version(version), m_pkg(pkg), m_dir(dir), 
+      m_fmxPath(fmxPath), m_srcPath(srcPath), m_fswId(fswId), m_status(status)
+    {};
+    void clear() {
+      m_key = m_name = m_version = m_pkg = m_dir = m_fmxPath = m_srcPath
+        = m_fswId = m_status = std::string("");
+    }
+    std::string getKey() const {return m_key;}
+    std::string getName() const {return m_name;}
+    std::string getVersion() const {return m_version;}
+    std::string getPkg() const {return m_pkg;}
+    std::string getDir() const {return m_dir;}
+    std::string getFmxPath() const {return m_fmxPath;}
+    std::string getSrcPath() const {return m_srcPath;}
+    std::string getFswId() const {return m_fswId;}
+    std::string getStatus() const {return m_status;}
+  private:
+    std::string m_key;
+    std::string m_name;
+    std::string m_version;
+    std::string m_pkg;
+    std::string m_dir;
+    std::string m_fmxPath;
+    std::string m_srcPath;
+    std::string m_fswId;
+    std::string m_status;
+  };
 
   /**
     Little utility class used by MootBuild and MootQuery.
--- src/MootQuery.cxx
+++ src/MootQuery.cxx
@@ -1,4 +1,4 @@
-//  $Header: /nfs/slac/g/glast/ground/cvs/mootCore/src/MootQuery.cxx,v 1.29 2007/12/19 19:33:55 jrb Exp $
+//  $Header: /nfs/slac/g/glast/ground/cvs/mootCore/src/MootQuery.cxx,v 1.30 2008/02/13 21:02:33 panetta Exp $
 
 #include <string>
 #include <cstdio>
@@ -670,6 +670,67 @@ namespace MOOT {
     }
     std::sort(parameterKeys.begin(), parameterKeys.end());
     return true;
+  }
+
+  /* ----------- */
+  ConstitInfo* MootQuery::getConstituentByFswId(unsigned fswId) {
+    std::string fswIdStr;
+    facilities::Util::utoa(fswId, fswIdStr);
+    std::string where(" WHERE FSW_id='");
+    where += fswIdStr + std::string("'");
+    return getConstituentWhere(where);
+  }
+
+  ConstitInfo* MootQuery::getConstituentInfo(unsigned key) {
+    std::string keyStr;
+    facilities::Util::utoa(key, keyStr);
+    std::string where(" WHERE prim_key= '");
+    where += keyStr + std::string("'");
+    return getConstituentWhere(where);
+  }
+
+
+  ConstitInfo* MootQuery::getConstituentWhere(const std::string& where) {
+
+    std::vector<std::string> getCols;
+    std::vector<std::string> noCols;
+    getCols.reserve(9);
+    getCols.clear(); noCols.clear();
+
+    getCols.push_back("prim_key");
+    getCols.push_back("name");
+    getCols.push_back("ver");
+    getCols.push_back("pkg");
+    getCols.push_back("dir");
+    getCols.push_back("fmx_path");
+    getCols.push_back("src_path");
+    getCols.push_back("FSW_id");
+    getCols.push_back("status");
+
+    rdbModel::ResultHandle* res = 0;
+
+    try {
+      res = m_rdb->getConnection()->select("Constituents", 
+                                           getCols, noCols, where);
+    }
+    catch (std::exception ex) {
+      std::cerr << "MootQuery::getConstituentWhere, where = " << where
+                << " SQL error: "  << ex.what() << std::endl;
+      std::cerr.flush();
+      if (res) delete res;
+      throw ex;
+    }
+    int n = res->getNRows();
+    if (!n) {
+      delete res;
+      return 0;
+    }
+    
+    std::vector<std::string>  flds;
+    res->getRow(flds, 0);
+
+    return new ConstitInfo(flds[0], flds[1], flds[2], flds[3], flds[4],
+                               flds[5], flds[6], flds[7], flds[8]);
   }
 
   unsigned MootQuery::getLastConfigKeyByAlg(const std::string& alg, 
