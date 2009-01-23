625b8ff1308bf4c9a0e6773b65fe4e1740abdfda
jrb <>
2009-01-23 19:57:10 +0000

    Add explicit references to xmlBaseLib, facilitiesLib since mootCore does directly use functions from these libraries

--- mootCoreLib.py
+++ mootCoreLib.py
@@ -1,7 +1,9 @@
-# $Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/mootCore/mootCoreLib.py,v 1.1 2008/08/15 21:42:39 ecephas Exp $
+# $Header: /nfs/slac/g/glast/ground/cvs/CHS-scons/mootCore/mootCoreLib.py,v 1.2 2008/08/25 20:58:10 ecephas Exp $
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['mootCore'])
     env.Tool('rdbModelLib')
+    env.Tool('xmlBaseLib')
+    env.Tool('facilitiesLib')
 def exists(env):
     return 1;
