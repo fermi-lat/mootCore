2c7e33c99e83529a83e529edbf9b2650eec6146d
jrb <>
2012-08-17 18:45:33 +0000

    patches for Windows no-install-headers

--- mootCoreLib.py
+++ mootCoreLib.py
@@ -1,7 +1,9 @@
-# $Header: /nfs/slac/g/glast/ground/cvs/CHS-scons/mootCore/mootCoreLib.py,v 1.2 2008/08/25 20:58:10 ecephas Exp $
+# $Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/mootCore/mootCoreLib.py,v 1.3 2009/01/23 19:57:10 jrb Exp $
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['mootCore'])
+        if env['PLATFORM']=='win32' and env.get('CONTAINERNAME','')=='GlastRelease':
+	    env.Tool('findPkgPath', package = 'mootCore') 
     env.Tool('rdbModelLib')
     env.Tool('xmlBaseLib')
     env.Tool('facilitiesLib')
